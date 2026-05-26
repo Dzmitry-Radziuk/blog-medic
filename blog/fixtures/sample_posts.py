4682e8efac261f95fca083797f7e2462ba427c66
Dzmitry Radziuk <mitia.radiuk@yandex.ru>
2026-05-26 17:45:55 +0300

    Update sample_posts.py

--- blog/fixtures/sample_posts.py
+++ blog/fixtures/sample_posts.py
@@ -5,33 +5,27 @@ import django
 from django.core.files import File
 from django.utils import timezone
 
-# --- 1. Добавляем корень проекта в sys.path ---
+from blog.models import Post
+
+
 BASE_DIR = os.path.dirname(
     os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 )
 sys.path.append(BASE_DIR)
-
-# --- 2. Настраиваем Django ---
 os.environ.setdefault(
     'DJANGO_SETTINGS_MODULE', 'config.settings'
-)  # <-- твой settings
-django.setup()  # <--- обязательно вызвать перед импортом моделей
-
-# --- 3. Импортируем модели ---
-from blog.models import Post
-
-# --- 4. Папка с картинками ---
+)
+django.setup()
 IMAGE_DIR = os.path.join(
     BASE_DIR, 'media', 'posts'
 )  # все файлы hero и контентные
 
-# --- 5. Данные статей ---
 sample_posts = [
     {
     },
 ]
 
-# --- 6. Сохраняем посты ---
+
 for post_data in sample_posts:
     image_name = post_data.pop("image", None)
     post, created = Post.objects.update_or_create(
